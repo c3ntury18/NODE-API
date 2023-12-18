01bf1d6553828479f217f3e3f82be3fd8881533e
c3ntury18 <paakwesiaidoo16@gmail.com>
2023-12-18 09:05:22 +0000

    Project

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app= express();
+const port = 3000;
 
 
-//routes
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
 
-app.get('/' , (req, res) => {
-res.send('Hello Node API')
+
+
+app.listen(3000, () => {
+    console.log('Server is ruuning on port 3000');
 })
 
-app.listen(3000, ()=> {
- console.log('Node API appis running on port 3000')
 
-})
